f5787089f2a6c65073511436b3f1db1fd7cb0fa1
osmanjosue <osmanjosue@gmail.com>
2024-02-27 11:46:29 -0600

    Clase: Refactorizar - Organizar lógicamente el código

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import { yarg } from "./config/plugins/args.plugin";
+import { ServerApp } from "./presentation/server-app";
 
 (async () => {
     await main();
 })();
 
 async function main() {
-    console.log(yarg)
+    
+    const {b:base, l:limit, s:showTable}= yarg;
+
+    ServerApp.run({base, limit, showTable});
 }
 
 /* console.log(process.argv); */
